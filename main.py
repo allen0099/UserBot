355f6efe00f95ca953f84e1cbe2b59331b5bb4f4
秉虎 <s96016641@gmail.com>
2020-02-07 18:11:02 +0800

    init

---
+++ main.py
@@ -0,0 +1,28 @@
+import logging
+import platform
+
+from pyrogram import Client
+from pyrogram.session import Session
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
+if __name__ == '__main__':
+    app = Client(
+        "bot",
+        app_version="Elegant 0.0.0.0",
+        device_model=platform.node(),
+        system_version=platform.system() + " " + platform.release()
+    )
+
+    try:
+        Session.notice_displayed = True
+        app.start()
+        log.debug("Client started successfully")
+        app.idle()
+        app.stop()
+    except:
+        log.critical("Client did not started successfully!")
