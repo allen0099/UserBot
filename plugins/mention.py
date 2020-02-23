eaadf592e360b6ad25bc73b2da20db144908f69d
秉虎 <s96016641@gmail.com>
2020-02-23 20:58:31 +0800

    refactor: rewrite mention admin
    rewrite mention admin function and remove mention everyone

--- plugins/mention.py
+++ plugins/mention.py
@@ -1,32 +1,24 @@
 import logging
+from typing import List
 
+import pyrogram
 from pyrogram import Client, Message, Filters
 
 log: logging.Logger = logging.getLogger(__name__)
-
-
-# TODO: rewrite this
-@Client.on_message(Filters.command("everyone", prefixes="@") & Filters.group & Filters.me)
-def mention_everyone(cli: Client, msg: Message) -> None:
-    cli.delete_messages(msg.chat.id, [msg.message_id])
-
-    members = cli.iter_chat_members(msg.chat.id)
-    string = ""
-    for i in members:
-        string += f"<a href='tg://user?id={i.user.id}'>" + "\u2060" + "</a>"
-    string += "@everyone"
-    cli.send_message(msg.chat.id, string, parse_mode="HTML",
-                     disable_notification=False)
 
 
 @Client.on_message(Filters.command("admin", "@") & Filters.group & Filters.me)
 def mention_admins(cli: Client, msg: Message) -> None:
     cli.delete_messages(msg.chat.id, [msg.message_id])
 
-    admins = cli.iter_chat_members(msg.chat.id, filter="Administrators")
-    string = ""
-    for i in admins:
-        string += f"<a href='tg://user?id={i.user.id}'>" + "\u2060" + "</a>"
-    string += "@admin"
-    cli.send_message(msg.chat.id, string, parse_mode="HTML",
+    admins: List[pyrogram.ChatMember] = \
+        cli.get_chat_members(msg.chat.id,
+                             filter="administrators")
+
+    string: str = "@admin "
+    for admin in admins:
+        string += f"<a href='tg://user?id={admin.user.id}'>" + "\u2060" + "</a>"
+    cli.send_message(msg.chat.id,
+                     string,
+                     parse_mode="HTML",
                      disable_notification=False)
