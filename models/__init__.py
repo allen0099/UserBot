ec1fe79bc36271946805bb349c87848f6889cccf
秉虎 <s96016641@gmail.com>
2020-06-15 03:02:26 +0800

    feat: disable sqlalchemy debug message
    disable sqlalchemy debug message

--- models/__init__.py
+++ models/__init__.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker, Session
 
 # https://stackoverflow.com/a/45613994
 
-engine: Engine = create_engine("sqlite:///settings.db", echo=True,
+engine: Engine = create_engine("sqlite:///settings.db",
                                connect_args=dict(check_same_thread=False))
 
 Base: DeclarativeMeta = declarative_base()
