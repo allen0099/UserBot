685c7bc6c36d488bcb76addc80e88cdd3f7e45f7
秉虎 <s96016641@gmail.com>
2020-07-02 11:18:16 +0800

    fix: no permission outside the group
    no permission to execute the command

--- bot/plugins/auth/command/bang.py
+++ bot/plugins/auth/command/bang.py
@@ -4,8 +4,8 @@ import time
 
 import pyrogram
 from pyrogram import Client, Message, Filters
-from pyrogram.api.functions import channels
 
+from bot.functions import have_permission, delete_all_msg
 from bot.plugins import COMMAND_PREFIX, LOG_CHANNEL
 from models import Users
 
@@ -23,11 +23,9 @@ def bang(cli: Client, msg: Message) -> None:
     reply_message: pyrogram.Message = msg.reply_to_message
     ban_user: pyrogram.User = msg.reply_to_message.from_user
 
-    permission: pyrogram.ChatMember = cli.get_chat_member(msg.chat.id, cli.get_me().id)
+    me: pyrogram.ChatMember = cli.get_chat_member(msg.chat.id, cli.get_me().id)
 
-    if permission.status == "administrator" and \
-            permission.can_delete_messages is True and \
-            permission.can_restrict_members is True:
+    if have_permission(me):
 
         if not reply_message.service:
             cli.forward_messages(LOG_CHANNEL, chat.id, reply_message.message_id)
@@ -47,14 +45,9 @@ def bang(cli: Client, msg: Message) -> None:
                       f"Banned user: <a href='tg://user?id={ban_user.id}'>{html.escape(str(ban_user.first_name))}</a>" \
                       f"({ban_user.id})"
         cli.send_message(LOG_CHANNEL, string)
-    # delete all message first
-    cli.send(
-        channels.DeleteUserHistory(
-            channel=cli.resolve_peer(chat.id),
-            user_id=cli.resolve_peer(ban_user.id)
-        )
-    )
-    # ban later
-    cli.kick_chat_member(chat.id, ban_user.id)
+        # delete all message first
+        delete_all_msg(cli, chat, ban_user)
+        # ban later
+        cli.kick_chat_member(chat.id, ban_user.id)
 
-    cli.delete_messages(chat.id, [msg.message_id, reply_message.message_id])
+        cli.delete_messages(chat.id, [msg.message_id, reply_message.message_id])
