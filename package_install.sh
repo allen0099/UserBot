4dba780835f66934a80159584d3a4dabbce1eb4f
秉虎 <s96016641@gmail.com>
2021-03-16 04:56:50 +0800

    build: update require

--- package_install.sh
+++ package_install.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+sudo apt install python3.9 python3.9-venv -y
+
 if [ ! -d "./venv" ]; then
-  python3 -m venv venv
+  python3.9 -m venv venv
   source venv/bin/activate
 
+  cp .env.example .env
   cp config.ini.example config.ini
 else
   echo "Virtual environment exist!"
 fi
 
-pip3 install -U pyrogram tgcrypto
+#sudo apt-get install gcc libpq-dev -y
+#sudo apt-get install python-dev  python-pip -y
+#sudo apt-get install python3-dev python3-pip python3-venv python3-wheel -y
+#pip3 install wheel
+
+pip install -r requirements.txt
+pip install -U SQLAlchemy
+pip install -U PyMySQL
+pip install -U python-dotenv
+pip install -U pyrogram tgcrypto
