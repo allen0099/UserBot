648ce35f4f6076dc90fa85a904ebebd693f8bfb9
秉虎 <s96016641@gmail.com>
2021-03-15 13:27:51 +0800

    feat: update User model and rewrite add user method

--- bot/functions/get_full.py
+++ bot/functions/get_full.py
@@ -63,38 +63,29 @@ async def refresh_user(cli: Client, peer: Union[InputPeerUser, InputPeerUserFrom
     cache_user: User = session.query(User).filter_by(uid=full_user.user.id).first()
 
     if cache_user is None:
-        cache_user: User = User(
-            full_user.user.id,
-            full_user.user.deleted,
-            full_user.user.bot,
-            full_user.user.verified,
-            full_user.user.restricted,
-            full_user.user.scam,
-            full_user.user.first_name,
-            full_user.user.last_name,
-            full_user.user.username,
-            full_user.user.phone,
-            full_user.user.lang_code,
-            full_user.common_chats_count,
-            full_user.about
-        )
-        session.add(cache_user)
-    else:
-        cache_user.deleted = full_user.user.deleted
-        cache_user.bot = full_user.user.bot
-        cache_user.verified = full_user.user.verified
-        cache_user.restricted = full_user.user.restricted
-        cache_user.scam = full_user.user.scam
-        cache_user.first_name = full_user.user.first_name
-        cache_user.last_name = full_user.user.last_name
-        cache_user.username = full_user.user.username
-        cache_user.phone = full_user.user.phone
-        cache_user.lang_code = full_user.user.lang_code
-        cache_user.common_chats_count = full_user.common_chats_count
-        cache_user.about = full_user.about
+        cache_user: User = User(full_user.user.id)
 
+    cache_user.dc_id = full_user.profile_photo.dc_id if full_user.profile_photo is not None else None
+    cache_user.first_name = full_user.user.first_name
+    cache_user.last_name = full_user.user.last_name if full_user.user.last_name is not None else None
+    cache_user.username = full_user.user.username if full_user.user.username is not None else None
+    cache_user.about = full_user.about
+    cache_user.bot = full_user.user.bot
+    cache_user.deleted = full_user.user.deleted
+    cache_user.verified = full_user.user.verified
+    cache_user.scam = full_user.user.scam
+    cache_user.support = full_user.user.support
+    cache_user.restricted = full_user.user.restricted
+    cache_user.phone_calls_available = full_user.phone_calls_available
+    cache_user.phone_calls_private = full_user.phone_calls_private
+    cache_user.video_calls_available = full_user.video_calls_available
+    cache_user.common_chats_count = full_user.common_chats_count
+
+    if cache_user in session.dirty:
         cache_user.expired_at = datetime.datetime.utcnow() + datetime.timedelta(hours=6)
-
-    session.commit()
+        session.commit()
+    else:
+        session.add(cache_user)
+        session.commit()
 
     return cache_user
--- database/models/User.py
+++ database/models/User.py
@@ -9,52 +9,25 @@ log: logging.Logger = logging.getLogger(__name__)
 
 
 class User(db.base, BaseMixin, TimestampMixin):
-    uid = Column(BigInteger)
-    deleted = Column(Boolean, nullable=True)
-    bot = Column(Boolean, nullable=True)
-    verified = Column(Boolean, nullable=True)
-    restricted = Column(Boolean, nullable=True)
-    scam = Column(Boolean, nullable=True)
+    uid: int = Column(BigInteger, nullable=False)
+    dc_id: int = Column(Integer)
+    first_name: str = Column(String(64), nullable=False)
+    last_name: str = Column(String(64))
+    username: str = Column(String(64))
+    about: str = Column(String(128), nullable=False)
 
-    first_name = Column(String(65))
-    last_name = Column(String(65))
-    username = Column(String(33))
-    phone = Column(String(100))
-    about = Column(String(71))
-    lang_code = Column(String(10))
+    bot: bool = Column(Boolean, nullable=False)
+    deleted: bool = Column(Boolean, nullable=False)
+    verified: bool = Column(Boolean, nullable=False)
+    scam: bool = Column(Boolean, nullable=False)
+    support: bool = Column(Boolean, nullable=False)
+    restricted: bool = Column(Boolean, nullable=False)
 
-    common_chats_count = Column(Integer)
+    phone_calls_available: bool = Column(Boolean, nullable=False)
+    phone_calls_private: bool = Column(Boolean, nullable=False)
+    video_calls_available: bool = Column(Boolean, nullable=False)
 
-    def __init__(
-            self,
-            uid: int,
-            deleted: bool = None,
-            bot: bool = None,
-            verified: bool = None,
-            restricted: bool = None,
-            scam: bool = None,
+    common_chats_count: int = Column(Integer, nullable=False)
 
-            first_name: str = None,
-            last_name: str = None,
-            username: str = None,
-            phone: str = None,
-            lang_code: str = None,
-
-            common_chats_count: int = None,
-            about: str = None
-    ):
+    def __init__(self, uid: int):
         self.uid = uid
-        self.deleted = deleted
-        self.bot = bot
-        self.verified = verified
-        self.restricted = restricted
-        self.scam = scam
-
-        self.first_name = first_name
-        self.last_name = last_name
-        self.username = username
-        self.phone = phone
-        self.lang_code = lang_code
-
-        self.common_chats_count = common_chats_count
-        self.about = about
