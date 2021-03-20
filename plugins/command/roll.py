86d424d9293d68338ced51db6e690abfb1e5cef7
秉虎 <s96016641@gmail.com>
2021-03-20 19:44:28 +0800

    feat: add random message

---
+++ plugins/command/roll.py
@@ -0,0 +1,17 @@
+import logging
+import random
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+log: logging.Logger = logging.getLogger(__name__)
+
+
+@Client.on_message(filters.command("random", prefixes="$") & filters.me)
+def roll_dice(_: Client, msg: Message) -> None:
+    if len(msg.command) == 1:
+        msg.reply(str(random.randint(1, 6)))
+    elif len(msg.command) == 2 and msg.command[1].isdigit():
+        msg.reply(str(random.randint(1, int(msg.command[1]))))
+    else:
+        msg.reply(random.choice(msg.command[1::]))
