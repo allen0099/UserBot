0aed49dad18c3d61524545ce4d8c72496af3e110
秉虎 <s96016641@gmail.com>
2021-03-20 18:49:18 +0800

    fix: fix auto report, still in test

--- plugins/auto/add_member_report.py
+++ plugins/auto/add_member_report.py
@@ -1,6 +1,5 @@
 import logging
 import os
-from typing import Optional
 
 from pyrogram import Client, filters
 from pyrogram.raw import functions
@@ -9,19 +8,21 @@ from pyrogram.types import Chat, Message, User
 log: logging.Logger = logging.getLogger(__name__)
 
 
-@Client.on_message(filters.new_chat_members)
-async def add_member_report(cli: Client, msg: Message) -> Optional[Message]:
-    # todo: check again
-    log.debug(f"{msg}")
-    return
+@Client.on_message(filters.new_chat_members & ~ filters.me)
+async def add_member_report(cli: Client, msg: Message) -> None:
     me: User = await cli.get_me()
-    if msg.from_user.id == me.id:
+    new_users: dict[int, User] = {i.id: i for i in msg.new_chat_members}
+
+    if msg.from_user.id in new_users.keys():
+        log.debug(f"User {msg.from_user.id} joined the group {msg.chat.title}({msg.chat.id}) via any, do nothing.")
         return
 
-    common_chats: list[Chat] = await cli.get_common_chats(msg.from_user.id)
+    inviter_common_groups: list[Chat] = await cli.get_common_chats(msg.from_user.id)
 
-    if len(common_chats) <= 3:
+    if me.id in new_users.keys() and len(inviter_common_groups) <= 3:
+        log.debug(f"What happened, I should leave {msg.chat.id} and report now.")
         if await cli.send(functions.messages.ReportSpam(peer=await cli.resolve_peer(msg.chat.id))):
-            await cli.leave_chat(msg.chat.id)
-            return await cli.send_message(os.getenv("log_channel"), f"#auto #leave #id{msg.chat.id}")
-        return await cli.send_message(os.getenv("log_channel"), f"#auto #leave #id{msg.chat.id} #failed")
+            await cli.send_message(os.getenv("log_channel"), f"#auto #leave #id{msg.chat.id}")
+            # await cli.leave_chat(msg.chat.id)
+        await cli.send_message(os.getenv("LOG_CHANNEL"), f"#id{msg.chat.id} #test #not_report")
+        await cli.send_message(os.getenv("LOG_CHANNEL"), f"<code>{msg}</code>")
