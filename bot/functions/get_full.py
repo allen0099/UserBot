9fd00a1b8d79a602b7671f095d95bb4a3f93ae5a
秉虎 <s96016641@gmail.com>
2021-03-28 01:34:01 +0800

    feat: make function belong to User model

--- bot/functions/get_full.py
+++ bot/functions/get_full.py
@@ -7,7 +7,7 @@ from pyrogram.errors import ChatAdminRequired, PeerIdInvalid
 from pyrogram.raw import functions
 from pyrogram.raw.base import InputPeer
 from pyrogram.raw.types import Channel, ChannelFull, ChatBannedRights, InputChannel, InputPeerChannel, \
-    InputPeerChannelFromMessage, InputPeerChat, InputPeerUser, InputPeerUserFromMessage, InputUser, UserFull
+    InputPeerChannelFromMessage, InputPeerChat, InputPeerUser, InputPeerUserFromMessage, InputUser
 from pyrogram.raw.types.messages import ChatFull
 from pyrogram.types import ChatMember
 from pyrogram.utils import get_channel_id
@@ -27,7 +27,7 @@ async def get_full(cli: Client, telegram_id: str) -> Union[DB_User, DB_Channel]:
         telegram_id: str = str(self.id)
 
     peer: InputPeer = await resolve_peer(cli, telegram_id)
-    log.debug(f"Peer: {type(peer)}")
+    log.debug(f"Peer instance: {type(peer)}")
 
     if isinstance(peer, (InputPeerUser, InputPeerUserFromMessage, InputUser)):
         return await get_user_full(cli, peer)
@@ -47,61 +47,14 @@ async def get_user_full(cli: Client, peer: Union[InputPeerUser, InputPeerUserFro
     cache_user: Optional[DB_User] = session.query(DB_User).filter_by(uid=uid).first()
 
     if cache_user is None:
-        log.debug(f"{uid} not cached")
-        return await refresh_user(cli, peer)
+        log.debug(f"{uid} not exist in database, creating...")
+        return await DB_User(uid).add(cli)
 
     if datetime.datetime.utcnow() < cache_user.expired_at:
         log.debug(f"{uid} cached")
         return cache_user
     log.debug(f"{uid} expired at {cache_user.expired_at}, now is {datetime.datetime.utcnow()}")
-    return await refresh_user(cli, peer)
-
-
-async def refresh_user(cli: Client, peer: Union[InputPeerUser, InputPeerUserFromMessage, InputUser]) -> DB_User:
-    session: Session = db.get_session()
-
-    log.debug(f"Refreshing {peer.user_id}")
-
-    full_user: UserFull = await cli.send(functions.users.GetFullUser(id=peer))
-    cache_user: DB_User = session.query(DB_User).filter_by(uid=full_user.user.id).first()
-
-    if cache_user is None:
-        cache_user: DB_User = DB_User(full_user.user.id)
-
-    cache_user.dc_id = full_user.profile_photo.dc_id if full_user.profile_photo is not None else None
-    cache_user.first_name = full_user.user.first_name
-    cache_user.last_name = full_user.user.last_name
-    cache_user.username = full_user.user.username
-    cache_user.about = full_user.about
-    cache_user.bot = full_user.user.bot
-    cache_user.deleted = full_user.user.deleted
-    cache_user.verified = full_user.user.verified
-    cache_user.scam = full_user.user.scam
-    cache_user.fake = full_user.user.fake
-    cache_user.support = full_user.user.support
-    cache_user.restricted = full_user.user.restricted
-    cache_user.phone_calls_available = full_user.phone_calls_available
-    cache_user.phone_calls_private = full_user.phone_calls_private
-    cache_user.video_calls_available = full_user.video_calls_available
-    cache_user.common_chats_count = full_user.common_chats_count
-
-    if full_user.user.bot:
-        cache_user.bot_info_version = full_user.user.bot_info_version
-        cache_user.bot_chat_history = full_user.user.bot_chat_history
-        cache_user.bot_nochats = full_user.user.bot_nochats
-        cache_user.bot_inline_geo = full_user.user.bot_inline_geo
-        cache_user.bot_inline_placeholder = full_user.user.bot_inline_placeholder
-        cache_user.bot_description = full_user.bot_info.description if full_user.bot_info else None
-
-    if cache_user in session.dirty:
-        cache_user.expired_at = datetime.datetime.utcnow() + datetime.timedelta(hours=6)
-        session.commit()
-    else:
-        session.add(cache_user)
-        session.commit()
-
-    ret: DB_User = session.query(DB_User).filter_by(uid=full_user.user.id).first()
-    return ret
+    return await cache_user.refresh(cli)
 
 
 async def get_channel_full(cli: Client,
--- database/models/User.py
+++ database/models/User.py
@@ -1,7 +1,15 @@
+import datetime
 import logging
 
+from pyrogram import Client
+from pyrogram.errors import PeerIdInvalid
+from pyrogram.raw import functions
+from pyrogram.raw.base import InputPeer
+from pyrogram.raw.types import InputPeerUser, InputPeerUserFromMessage, InputUser, UserFull
 from sqlalchemy import BigInteger, Boolean, Column, Integer, String
+from sqlalchemy.orm import Session
 
+from bot.util import resolve_peer
 from database.mixin import BaseMixin, TimestampMixin
 from main import db
 
@@ -39,3 +47,59 @@ class User(db.base, BaseMixin, TimestampMixin):
 
     def __init__(self, uid: int):
         self.uid = uid
+
+    async def refresh(self, cli: Client) -> "User":
+        session: Session = db.get_session()
+        uid = self.uid
+
+        log.debug(f"Refreshing {uid}")
+        peer: InputPeer = await resolve_peer(cli, uid)
+
+        if not isinstance(peer, (InputPeerUser, InputPeerUserFromMessage, InputUser)):
+            raise PeerIdInvalid
+
+        full_user: UserFull = await cli.send(functions.users.GetFullUser(id=peer))
+        cache_user: User = session.query(User).filter_by(uid=full_user.user.id).first()
+
+        if cache_user is None:
+            cache_user: User = User(full_user.user.id)
+
+        cache_user.dc_id = full_user.profile_photo.dc_id if full_user.profile_photo is not None else None
+        cache_user.first_name = full_user.user.first_name
+        cache_user.last_name = full_user.user.last_name
+        cache_user.username = full_user.user.username
+        cache_user.about = full_user.about
+        cache_user.bot = full_user.user.bot
+        cache_user.deleted = full_user.user.deleted
+        cache_user.verified = full_user.user.verified
+        cache_user.scam = full_user.user.scam
+        cache_user.fake = full_user.user.fake
+        cache_user.support = full_user.user.support
+        cache_user.restricted = full_user.user.restricted
+        cache_user.phone_calls_available = full_user.phone_calls_available
+        cache_user.phone_calls_private = full_user.phone_calls_private
+        cache_user.video_calls_available = full_user.video_calls_available
+        cache_user.common_chats_count = full_user.common_chats_count
+
+        if full_user.user.bot:
+            cache_user.bot_info_version = full_user.user.bot_info_version
+            cache_user.bot_chat_history = full_user.user.bot_chat_history
+            cache_user.bot_nochats = full_user.user.bot_nochats
+            cache_user.bot_inline_geo = full_user.user.bot_inline_geo
+            cache_user.bot_inline_placeholder = full_user.user.bot_inline_placeholder
+            cache_user.bot_description = full_user.bot_info.description if full_user.bot_info else None
+
+        if cache_user in session.dirty:
+            cache_user.expired_at = datetime.datetime.utcnow() + datetime.timedelta(hours=6)
+            session.commit()
+        else:
+            session.add(cache_user)
+            session.commit()
+
+        log.debug(f"Refreshed user {uid}!")
+        ret: User = session.query(User).filter_by(uid=full_user.user.id).first()
+        return ret
+
+    async def add(self, cli: Client) -> "User":
+        log.debug(f"Adding new user: {self.uid}")
+        return await self.refresh(cli)
