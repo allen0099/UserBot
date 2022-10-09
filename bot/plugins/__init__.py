d02c2c0b193d9e349ec69314897ba3163e502cc8
秉虎 <s96016641@gmail.com>
2022-10-10 03:28:30 +0800

    Feat: Split bot file and rewrites
    - Rewrite delete range messages
    - Rewrite get chat admins
    - Rewrite admin required filter

--- bot/plugins/__init__.py
+++ bot/plugins/__init__.py
@@ -3,4 +3,5 @@ import logging
 from core.log import main_logger
 
 log: logging.Logger = main_logger(__name__)
+
 COMMAND_PREFIXES: list[str] = ["!", "$"]
--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,6 @@
+import logging
+
+from core.log import main_logger
 from . import settings
+
+log: logging.Logger = main_logger(__name__)
