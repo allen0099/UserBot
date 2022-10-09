6f7c6412420d12f61c6c1793d2d57c4f66be8ca0
秉虎 <s96016641@gmail.com>
2022-10-10 03:32:03 +0800

    Feat: Add Emoji enumerate

--- bot/enums.py
+++ bot/enums.py
@@ -14,3 +14,22 @@ class ActionsList(Enum):
     DELETE_ONE = auto()
     DELETE_ALL_AND_BAN = auto()
     DO_NOTHING = auto()
+
+
+class EmojiList(str, Enum):
+    EMPTY = ""
+    TRUE = "✅"
+    FALSE = "❎"
+
+    DENY = "😢"
+    INFO = "ℹ"
+    DELETE = "🗑️"
+    BAN = "🚫"
+    ADD_MEMBER = "🔗"
+    PIN = "📌"
+    VOICE = "🔊"
+    ADD_ADMIN = "➕"
+
+    BOT = "🤖"
+    BOT_EYES = "👀"
+    BOT_CLOSE_EYES = "🕶"
