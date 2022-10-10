2abcfb8e43ba03093ed897374df5feaf7881efd7
秉虎 <s96016641@gmail.com>
2022-10-10 21:54:10 +0800

    Feat: Add roll and move ping to self only

--- bot/plugins/me_only/ping.py
+++ bot/plugins/me_only/ping.py
@@ -14,18 +14,8 @@ log: logging.Logger = main_logger(__name__)
 # 先合併裡面的，回傳 func 往上丟，每一次合併記憶體位置都會不一樣
 # 因此，Client.on_message 必須是最外層的 decorator，才能確保前面的東西能被順利執行
 @Client.on_message(
-    filters.command("ping", prefixes=COMMAND_PREFIXES) & ~filters.forwarded
+    filters.command("ping", prefixes=COMMAND_PREFIXES) & filters.me & ~filters.forwarded
 )
 @event_log()
 async def ping(_: Bot, msg: Message) -> None:
-    """
-    Return pong.
-
-    Args:
-        _:
-        msg:
-
-    Returns:
-        None
-    """
     await msg.reply_text("pong!")
