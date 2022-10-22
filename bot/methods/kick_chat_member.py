33f39e49183933ac79e07ed2736ac928b9165401
秉虎 <s96016641@gmail.com>
2022-10-22 15:59:02 +0800

    Fix: Let kicked user ban 1 minute, it will auto release by Telegram

--- bot/methods/kick_chat_member.py
+++ bot/methods/kick_chat_member.py
@@ -1,17 +1,21 @@
 import asyncio
+from datetime import datetime, timedelta
 
 from pyrogram.errors import FloodWait
 
 import bot
+from core.settings import TIMEZONE
 
 
 class KickChatMember:
     async def kick_chat_member(
         self: "bot.Bot", chat_id: int | str, user_id: int | str
     ) -> bool:
+        ban_time: datetime = datetime.now(TIMEZONE)
+        ban_time += timedelta(minutes=1)
+
         try:
-            await self.ban_chat_member(chat_id, user_id)
-            await self.unban_chat_member(chat_id, user_id)
+            await self.ban_chat_member(chat_id, user_id, ban_time)
 
         except FloodWait as error:
             await asyncio.sleep(error.value)
